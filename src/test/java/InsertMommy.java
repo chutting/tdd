c984f316df3e89b61bee85ba12c93d2c14ed3044
chutting <tingting.chu@thoughtworks.com>
2020-09-14 12:16:06 +0800

    [Chu] feat: complete the test string_contains_repeat_vowels_and_repeat_vowels_are_less_than_30_percent_of_string

--- src/test/java/InsertMommy.java
+++ src/test/java/InsertMommy.java
@@ -1,6 +1,4 @@
 import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class InsertMommy {
   public String insert(String originalString) {
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -9,4 +9,12 @@ public class MommifierTest {
     String outputStr = insertMommy.insert(inputString);
     assertEquals("kkemommyemommyemommyep", outputStr);
   }
+
+  @Test
+  void string_contains_repeat_vowels_and_repeat_vowels_are_less_than_30_percent_of_string() {
+    InsertMommy insertMommy = new InsertMommy();
+    String inputString = "kkkkeep";
+    String outputStr = insertMommy.insert(inputString);
+    assertEquals(inputString, outputStr);
+  }
 }
